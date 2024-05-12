4ea0f6077c463ee1b49465b13bf01911a5c9bb99
Chito <remote.alfon@gmail.com>
2024-05-12 17:18:14 +0800

    getting light values

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import rcu
 
 
-# The robot class
-class RobotController:
+# All about motor controller
+class MotorController:
     @staticmethod
     def set_motor_straight(speed):
         rcu.SetMotor(1, speed)
@@ -13,9 +13,25 @@ class RobotController:
         rcu.SetWaitForTime(1)
 
 
+# All about color controller
+class ColorController:
+    _yellowColor = 0xFFE0
+    _blackColor = 0x0000
+
+    @staticmethod
+    def display_color_in_screen():
+        while True:
+            display = str(rcu.GetLightSensor(1)) + " " + str(rcu.GetLightSensor(2)) + " " + str(
+                rcu.GetLightSensor(3)) + " " + str(rcu.GetLightSensor(4)) + " " + str(
+                rcu.GetLightSensor(5)) + " " + str(rcu.GetLightSensor(6)) + " " + str(rcu.GetLightSensor(7))
+            rcu.SetDisplayString(1, display, ColorController._yellowColor, ColorController._blackColor)
+            rcu.SetWaitForTime(0.5)
+
+
 # entry point
 def main():
-    RobotController.set_motor_straight(50)
+    # MotorController.set_motor_straight(50)
+    ColorController.display_color_in_screen()
 
 
 main()
