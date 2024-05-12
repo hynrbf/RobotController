aae4774e67752a4f0cb5fc3e7558b4566f254947
Chito <remote.alfon@gmail.com>
2024-05-12 14:27:05 +0800

    update

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import rcu
 # The robot class
 class RobotController:
     @staticmethod
-    def set_motor_straight(motor_no_1, motor_no_2, speed):
-        rcu.SetMotorStraight(motor_no_1, motor_no_2, speed)
+    def set_motor_straight(speed):
+        rcu.SetMotor(1, speed)
+        rcu.SetMotor(2, speed)
+        rcu.SetWaitForTime(1)
+        rcu.SetMotor(1, -speed)
+        rcu.SetMotor(2, -speed)
+        rcu.SetWaitForTime(1)
 
 
 # entry point
 def main():
-    RobotController.set_motor_straight(1, 2, 300)
+    RobotController.set_motor_straight(50)
 
 
-if __name__ == "__main__":
-    main()
+main()
