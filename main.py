64d5ec0a3008ba40655e2ca98b9d159f896eb412
Chito <remote.alfon@gmail.com>
2024-05-12 17:55:32 +0800

    fix color

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+# read here first, https://geekdaxue.co/read/zmrobo@robocode/elb5c7#5kyz7i
 import rcu
+
+
+class SensorColors:
+    Red = 1
+    Green = 2
+    Blue = 3
+    Yellow = 4
+    Black = 5
+    White = 6
 
 
 # All about motor controller
@@ -15,16 +25,26 @@ class MotorController:
 
 # All about color controller
 class ColorController:
-    _yellowColor = 0xFFE0
-    _blackColor = 0x0000
+    _displayTextYellowColor = 0xFFE0
+    _screenBackgroundBlackColor = 0x0000
 
     @staticmethod
     def display_color_in_screen():
         while True:
-            display = str(rcu.GetLightSensor(1)) + " " + str(rcu.GetLightSensor(2)) + " " + str(
-                rcu.GetLightSensor(3)) + " " + str(rcu.GetLightSensor(4)) + " " + str(
-                rcu.GetLightSensor(5)) + " " + str(rcu.GetLightSensor(6)) + " " + str(rcu.GetLightSensor(7))
-            rcu.SetDisplayString(1, display, ColorController._yellowColor, ColorController._blackColor)
+            display = "Color detected is not either black or white"
+
+            if rcu.GetColorSensor(1, 4) == SensorColors.Black:
+                display = "Black color detected"
+                rcu.SetDisplayString(1, display, ColorController._displayTextYellowColor,
+                                     ColorController._screenBackgroundBlackColor)
+                continue
+
+            if rcu.GetColorSensor(1, 4) == SensorColors.White:
+                display = "White color detected"
+                rcu.SetDisplayString(1, display, ColorController._displayTextYellowColor,
+                                     ColorController._screenBackgroundBlackColor)
+                continue
+
             rcu.SetWaitForTime(0.5)
 
 
