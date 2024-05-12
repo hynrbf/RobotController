487503beed26f0b8ed8f23e981b207b81e854d1f
Chito <remote.alfon@gmail.com>
2024-05-13 00:21:21 +0800

    add intelligent plaform

--- main.py
+++ main.py
@@ -68,13 +68,52 @@ class ColorController:
 
             rcu.SetWaitForTime(0.5)
 
+    @staticmethod
+    def display_intelligent_tracking_module_values_in_screen():
+        standard_channel_1 = 130
+        standard_channel_two = 113
+        # ToDo. continue and use these values like the above
+        st_channel_three = 103
+        st_channel_four = 105
+        st_channel_five = 112
+        st_channel_six = 118
+        st_channel_seven = 101
+
+        while True:
+            actual_channel_1 = rcu.GetTraceV2I2C(1, 1)
+            actual_channel_2 = rcu.GetTraceV2I2C(1, 2)
+
+            display = str(actual_channel_1) + " " + str(actual_channel_2) + " " + str(
+                rcu.GetTraceV2I2C(1, 3)) + " " + str(rcu.GetTraceV2I2C(1, 4)) + " " + str(
+                rcu.GetTraceV2I2C(1, 5)) + " " + str(rcu.GetTraceV2I2C(1, 6)) + " " + str(rcu.GetTraceV2I2C(1, 7))
+            rcu.SetDisplayString(1, display, ColorController._displayTextYellowColor,
+                                 ColorController._screenBackgroundBlackColor)
+
+            display_2 = "values: "
+
+            if actual_channel_1 > standard_channel_1:
+                display_2 = display_2 + "light, "
+            else:
+                display_2 = display_2 + "dark, "
+
+            if actual_channel_2 > standard_channel_two:
+                display_2 = display_2 + "light, "
+
+            else:
+                display_2 = display_2 + "dark, "
+
+            rcu.SetDisplayString(20, display_2, ColorController._displayTextYellowColor,
+                                 ColorController._screenBackgroundBlackColor)
+            rcu.SetWaitForTime(0.1)
+
 
 # entry point
 def main():
     # MotorController.set_motor_straight(50)
     # just select between the two both are working
-    ColorController.display_color_in_screen()
+    # ColorController.display_color_in_screen()
     # ColorController.display_photo_electric_values_in_screen()
+    ColorController.display_intelligent_tracking_module_values_in_screen()
 
 
 main()
